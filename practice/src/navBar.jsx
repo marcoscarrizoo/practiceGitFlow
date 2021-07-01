a4c180c86ade0fa967d7fcad7fc71b68006ea232
briancalamar <briansantaran@gmail.com>
2021-07-01 20:43:17 -0300

    agregue rama

--- practice/src/navBar.jsx
+++ practice/src/navBar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const navBar = () => {
     return (
         <div>
+            <h1>
             navbar!!!
+            </h1>
+            <h1> segundo cambio de brian</h1>
         </div>
     )
 }
